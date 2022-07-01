015d0f65c4de82f3cbe04e25de0845677a3f6a81
Arllette Fonseca <arllette.fonseca@gmail.com>
2022-07-01 14:28:34 -0700

    working on morra backend

--- Morra/index.mjs
+++ Morra/index.mjs
@@ -1,50 +1,50 @@
-//front end
-//load library
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+// //front end
+// //load library
+// import { loadStdlib } from '@reach-sh/stdlib';
+// import * as backend from './build/index.main.mjs';
 
-//assign variable to standard library
-const stdlib = loadStdlib();
+// //assign variable to standard library
+// const stdlib = loadStdlib();
 
 
-//starting balance of each participant
-const startingBalance = stdlib.parseCurrency(100);
-//giving each participant their balances
-const accAlice = await stdlib.newTestAccount(startingBalance);
-const accBob = await stdlib.newTestAccount(startingBalance);
-//format currency and go to 4 decimal places
-const fmt = (x) => stdlib.formatCurrency(x,4);
-//function for getting the balance of a participant and displaying it with up to 4 decimals
-const getBalance = async(who) => fmt(await stdlib.balanceOf(who))
-/** get balance from each before game start here */
+// //starting balance of each participant
+// const startingBalance = stdlib.parseCurrency(100);
+// //giving each participant their balances
+// const accAlice = await stdlib.newTestAccount(startingBalance);
+// const accBob = await stdlib.newTestAccount(startingBalance);
+// //format currency and go to 4 decimal places
+// const fmt = (x) => stdlib.formatCurrency(x,4);
+// //function for getting the balance of a participant and displaying it with up to 4 decimals
+// const getBalance = async(who) => fmt(await stdlib.balanceOf(who))
+// /** get balance from each before game start here */
 
-//set up contract
-//Alice deploys contract
-const ctcAlice= accAlice.contract(backend);
-//bob attaches to Alice's contract to get information to interact
-const ctcBob= accBob.contract(ctcAlice.getInfo());
+// //set up contract
+// //Alice deploys contract
+// const ctcAlice= accAlice.contract(backend);
+// //bob attaches to Alice's contract to get information to interact
+// const ctcBob= accBob.contract(ctcAlice.getInfo());
 
-//logic for game play
-const FINGERS= ['1', '2', '3','4', '5'];
-const SUM = ['1', '2', '3', '4', '5', '6', '7', '8','9', '10'];
-const OUTCOME =['Alice Wins', 'Bob Wins', 'Draw'];
+// //logic for game play
+// const FINGERS= ['1', '2', '3','4', '5'];
+// const SUM = ['1', '2', '3', '4', '5', '6', '7', '8','9', '10'];
+// const OUTCOME =['Alice Wins', 'Bob Wins', 'Draw'];
 
-//method functionality
-const Player = (who) => ({
-  ...stdlib.hasRandom,
-  getFingers: ()=> {
-    //randomly select up to 5 fingers
-    const finger = Math.floor(Math.random() * 5);
-    console.log(`${who} played finger ${FINGERS[finger]} `);
-  },
-  getSum: () => {
-    //randomly choose sum
-    const sum = Math.floor(Math.random() * 10 );
-    console.log(`${who} played sum ${SUM[sum]}`);
-  },
-  //takes in one parameter
-  seeOutcome: (outcome) => {
-    console.log(`${who} saw outcome ${OUTCOME[outcome]}`)
-  }
-  /** add timeout method */
-})
+// //method functionality
+// const Player = (who) => ({
+//   ...stdlib.hasRandom,
+//   getFingers: ()=> {
+//     //randomly select up to 5 fingers
+//     const finger = Math.floor(Math.random() * 5);
+//     console.log(`${who} played finger ${FINGERS[finger]} `);
+//   },
+//   getSum: () => {
+//     //randomly choose sum
+//     const sum = Math.floor(Math.random() * 10 );
+//     console.log(`${who} played sum ${SUM[sum]}`);
+//   },
+//   //takes in one parameter
+//   seeOutcome: (outcome) => {
+//     console.log(`${who} saw outcome ${OUTCOME[outcome]}`)
+//   }
+//   /** add timeout method */
+// })
--- Morra/index.rsh
+++ Morra/index.rsh
@@ -5,20 +5,51 @@
 const Player = {
  //getFingers has no parameters and outputs numbers
  getFingers: Fun([], UInt),
- getSum: Fun([], UInt),
+ //get guess of what the sum will be no parameter and outputs numbers
+ getGuess: Fun([], UInt),
  //seeOutcome takes in integer and has no output
  seeOutcome: Fun(UInt, null)
 
 }
 //initialize participants
 export const main = Reach.app(() => {
-  const Alice = Participant('Alice', {
+  const Alice = Participant("Alice", {
     //interact interface
     ...Player,
   });
-  const Bob = Participant('Bob', {
+  const Bob = Participant("Bob", {
     //interact interface
     ...Player,
-    })
+  });
+  //intitialize app
+  init();
+  //here both Alice and Bob are in step they then move to their local step(local machine) with the .only method
 
+  //in local step Alice interacts with the getFingers method and the getGuess methods, she then delcassifies
+
+  Alice.only(() => {
+    const fingersAlice = declassify(interact.getFingers());
+    const guessAlice = delclassify(interact.getGuess());
+    //after declassifying Alice moves back to step
+  });
+  //Alice publishes fingers and guess at the same time, publishing moves her to consensus step
+  //consensus step happens on blockchain and is immutable
+  Alice.publish(fingersAlice);
+  Alice.publish(guessAlice);
+  //she then commits and moves back to step
+  commit();
+  /* QUESTION: once you publish and move to consensus is it immutable there or only after you commit??? */
+  //here bob moves to local step using .only method
+  Bob.only(() => {
+    const fingersBob = declassify(interact.getFingers());
+    const guessBob = declassify(interact.getGuess());
+    //he interacts with the methods and declassifies which moves him back to step
+
+  });
+  //he then publishes his answers and moves to consensus step
+  //consensus step happens on blockchain and is immutable
+  Bob.publish(fingersBob);
+  Bob.publish(guessBob);
+  //he then commits the publishes and moves back to step
+  commit();
 });
